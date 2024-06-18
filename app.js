abd78e16c124654cad8f7daa8a0c49425446b8be
sanketmctrl <138205583+sanketmctrl@users.noreply.github.com>
2024-06-18 15:42:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import { connect } from "puppeteer-real-browser";
+import fs from 'fs';
+import path from 'path';
 
 const getImage = async () => {
   const url = process.env.URL || "";
@@ -14,24 +16,40 @@ const getImage = async () => {
   const { browser, page } = response;
   console.log("Received Browser");
   console.log("Moving to page..");
-  page.goto(url, { waitUntil: "domcontentloaded" });
-  const ss = await getScreenshot(page,browser);
-  console.log("screenshot successfull, size of base64 ",ss.length);
-  return ss;
+  await page.goto(url, { waitUntil: "domcontentloaded" });
+  const ss = await getScreenshot(page, browser);
+  console.log("Screenshot successful, size of base64", ss.length);
+  await saveScreenshot(ss);
+  console.log("Screenshot saved");
 };
 
-const getScreenshot = (page,browser) => {
+const getScreenshot = (page, browser) => {
   return new Promise((resolve) => {
     setTimeout(async () => {
       const ss = await page.screenshot({
-        fullMode: true,
+        fullPage: true,
       });
-      console.log("almost there...");
+      console.log("Almost there...");
       const ssBase64 = ss.toString("base64");
       await browser.close();
       resolve(ssBase64);
     }, 10000);
-  })
-}
+  });
+};
+
+const saveScreenshot = (base64) => {
+  return new Promise((resolve, reject) => {
+    const filePath = path.join('/pp', 'screenshot.txt');
+    fs.writeFile(filePath, base64, 'utf8', (err) => {
+      if (err) {
+        console.error("Failed to save screenshot:", err);
+        reject(err);
+      } else {
+        console.log("Screenshot saved successfully to", filePath);
+        resolve();
+      }
+    });
+  });
+};
 
 getImage();
