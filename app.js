0a4884b7e359b24e458d328d20c414ca002086eb
tushar <tushar@missionctrl.dev>
2024-06-19 14:11:22 +0530

    Converted into API

--- app.js
+++ app.js
@@ -1,75 +1,56 @@
+import express from "express";
 import { connect } from "puppeteer-real-browser";
-import fs from "fs";
-import path from "path";
+
+const app = express();
 const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
 
-const getImage = async () => {
-  const url = process.env.URL || "https://www.stake.com";
-  if (!url) {
-    console.error("URL environment variable is not set!");
-    process.exit(1);
-  }
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const getScreenshot = async (url) => {
   const response = await connect({
     headless: "auto",
     fingerprint: true,
     turnstile: true,
+    connectOption: {
+      protocolTimeout: 1800000,
+    },
   });
+
   const { browser, page } = response;
-  console.log("Received Browser");
-  console.log("Moving to page..");
+
   try {
-    await page.goto(url, { waitUntil: "networkidle0" });
-    const ss = await getScreenshot(page);
-    console.log("Screenshot successful, size of base64", ss.length);
-    await saveScreenshot(ss);
-    console.log("Screenshot saved");
-  } catch (error) {
-    console.error("Failed to capture screenshot:", error);
+    await page.goto(url, { waitUntil: "domcontentloaded" });
+    await sleep(10000);
+
+    const screenshot = await page.screenshot({
+      fullPage: true,
+      encoding: "base64",
+    });
+
+    return screenshot;
   } finally {
     await browser.close();
   }
 };
 
-const getScreenshot = async (page) => {
-  try {
-    console.log(
-      "We are processing to capture a screenshot, please wait for 10 seconds..."
-    );
-    await sleep(10000);
-    const screenshot = await page.screenshot({
-      fullPage: true,
-      path: "test.png",
-    });
-    console.log("Captured...");
-    return screenshot.toString("base64");
-  } catch (error) {
-    console.error("Error while taking screenshot:", error);
-    throw error;
+app.get("/screenshot", async (req, res) => {
+  const { url } = req.query;
+  console.log(url);
+  if (!url) {
+    return res.status(400).json({ error: "URL parameter is required" });
   }
-};
+  try {
+    const screenshotBase64 = await getScreenshot(url.toString());
+    res.status(200).json({ image: screenshotBase64 });
+  } catch (error) {
+    console.error("Failed to capture screenshot:", error);
+    res.status(500).json({ error: "Failed to capture screenshot" });
+  }
+});
 
-const saveScreenshot = (base64) => {
-  return new Promise((resolve, reject) => {
-    const dirPath = "/pp";
-    const filePath = path.join(dirPath, "screenshot.txt");
+const PORT = process.env.PORT || 3000;
 
-    fs.mkdir(dirPath, { recursive: true }, (err) => {
-      if (err) {
-        console.error("Failed to create directory:", err);
-        reject(err);
-      } else {
-        fs.writeFile(filePath, base64, "utf8", (err) => {
-          if (err) {
-            console.error("Failed to save screenshot:", err);
-            reject(err);
-          } else {
-            console.log("Screenshot saved successfully to", filePath);
-            resolve();
-          }
-        });
-      }
-    });
-  });
-};
-
-getImage();
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- misc/register-journey.js
+++ misc/register-journey.js
@@ -1,14 +1,19 @@
 import { launch } from "puppeteer";
+const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+
 const main = async () => {
   const browser = await launch({
     headless: false,
+    args: ["--start-fullscreen"],
   });
   const page = await browser.newPage();
-  await page.goto("https://club5.high5casino.com/");
+  await page.setViewport({ width: 1920, height: 1080 });
+  await page.goto("https://www.williamhill.com");
+  // await page.goto("https://club5.high5casino.com/");
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36"
   );
-  await page.screenshot({ path: "before_click.png" });
+  await page.screenshot({ path: "before_click.png", fullPage: true });
   await page.waitForNetworkIdle();
   await page.evaluate(() => {
     const buttons = Array.from(document.querySelectorAll("button, a"));
@@ -16,15 +21,26 @@ const main = async () => {
       (button) =>
         button.innerText.toLowerCase().includes("register") ||
         button.innerText.toLowerCase().includes("sign up") ||
+        button.innerText.toLowerCase().includes("join") ||
         button.innerText.toLowerCase().includes("registration")
     );
     if (registerButton) {
       registerButton.click();
     }
   });
-
-  await page.waitForNavigation();
+  console.log("Got the register");
+  await sleep(5000);
   await page.screenshot({ path: "after_click.png" });
+
+  await page.evaluate(() => {
+    const inputs = document.querySelectorAll("input");
+    inputs.forEach((input) => {
+      input.focus();
+    });
+  });
+
+  await sleep(2000);
+  await page.screenshot({ path: "after_inputs_click.png", fullPage: true });
 
   await browser.close();
 };
